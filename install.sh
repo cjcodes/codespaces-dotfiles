102484a038db3f7ede13f198db7f92612428086c
Chris Johnson <chris.johnson@vanta.com>
2025-07-29 11:02:59 +0100

    fix typo

--- install.sh
+++ install.sh
@@ -12,8 +12,8 @@ sudo apt update
 sudo apt install -y ${PACKAGES[@]}
 
 wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.tar.gz
-tar xzvf nvim-linux-x86_64.tar.gz nvim
-sudo mv nvim /opt/nvim
+tar xzvf nvim-linux-x86_64.tar.gz
+sudo mv nvim-linux-x86_64 /opt/nvim
 sudo ln -s /opt/nvim/bin/nvim /usr/local/bin/nvim
 
 if [ ! -d ~/.config/nvim ]; then
